71ffd920143d740894d674f5c62ad004e97438ed
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-05-05 14:41:40 -0600

    se agrego una funcion para escribir el resultado de una funcion en un archivo txt

--- 03Bases/app.js
+++ 03Bases/app.js
@@ -1,7 +1,17 @@
+const { error } = require('console');
+const file=require('fs')
+
 console.clear();
 
 let base = 5
+let salida = ''
 
 for(let i=0; i<=10; i++){
-    console.log(`${base} x ${i} = ${i*base}`)
+    salida += `${base} x ${i} = ${i*base}\n`
+    //console.log(`${base} x ${i} = ${i*base}`)
 }
+
+file.writeFile( `tabla${base}.txt`,salida, (error)=>{
+    if (error) throw error
+    console.log('archivo creado')
+})
