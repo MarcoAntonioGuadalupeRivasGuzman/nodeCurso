f5eb780f1f8f7fbf5683f25ef2c95f0b1c5c6652
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-05-08 21:01:06 -0600

    se importo yargs

--- 03Bases/app.js
+++ 03Bases/app.js
@@ -1,21 +1,25 @@
 const {createFile} = require('./helpers/multiplicar')
+const argv = require('yargs').argv;
 
 console.clear();
 
-const [ , ,arg3='base:5']=process.argv
-const [, base]=arg3.split(':');
+//const [ , ,arg3='base:5']=process.argv
+//const [, base]=arg3.split(':');
 //console.log(base)
 
 //let base = 3
 let salida = ''
 
-for(let i=0; i<=10; i++){
+/*for(let i=0; i<=10; i++){
     salida += `${base} x ${i} = ${i*base}\n`
     //console.log(`${base} x ${i} = ${i*base}`)
-}
+}*/
 
-createFile(salida,base)
+/*createFile(salida,base)
     .then(nombreArchivo => console.log(nombreArchivo, 'creado'))
-    .catch(err => console.log(err))
+    .catch(err => console.log(err))*/
 
-console.log('archivo creado')
+//console.log('archivo creado')
+
+console.log(process.argv)
+console.log(argv)
