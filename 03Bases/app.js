3b4faf4aff5807bd8d029da1bd27f37f0ad56c8d
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-05-10 22:03:12 -0600

    se creo una configuracion para una variable que imprime la tabla por consola

--- 03Bases/app.js
+++ 03Bases/app.js
@@ -1,18 +1,24 @@
 const { options } = require('yargs');
-const {createFile} = require('./helpers/multiplicar')
+const { createFile } = require('./helpers/multiplicar')
 const argv = require('yargs')
-.option('b',{
-    alias: 'base',
-    type: 'number',
-    demandOption: true
-})
-.check((argv, options)=>{
-    if(isNaN(argv.base)){
-        throw 'La base debe ser un numero'
-    }
-    return true
-})
-.argv;
+    .option('b', {
+        alias: 'base',
+        type: 'number',
+        demandOption: true
+    })
+    .option('l', {
+        alias: 'listar',
+        type: 'boolean',
+        demandOption: true,
+        default: true
+    })
+    .check((argv, options) => {
+        if (isNaN(argv.base)) {
+            throw 'La base debe ser un numero'
+        }
+        return true
+    })
+    .argv;
 
 console.clear();
 
@@ -23,12 +29,14 @@ console.clear();
 //let base = 3
 let salida = ''
 
-for(let i=0; i<=10; i++){
-    salida += `${argv.b} x ${i} = ${i*argv.b}\n`
-    //console.log(`${base} x ${i} = ${i*base}`)
+for (let i = 0; i <= 10; i++) {
+    salida += `${argv.b} x ${i} = ${i * argv.b}\n`
+    if (argv.l) {
+        console.log(`${argv.b} x ${i} = ${i * argv.b}`)
+    }
 }
 
-createFile(salida,argv.b)
+createFile(salida, argv.b)
     .then(nombreArchivo => console.log(nombreArchivo, 'creado'))
     .catch(err => console.log(err))
 
